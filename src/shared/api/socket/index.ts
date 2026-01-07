772e18cafc83408b7a435e963ee4b845c0b849e5
DeadInsideDi <matvei778990@gmail.com>
2026-01-07 19:44:12 +0300

    return to initial

--- src/shared/api/socket/index.ts
+++ src/shared/api/socket/index.ts
@@ -1,9 +1,9 @@
 import { SOCKET_URL } from '@/shared/config'
 import { type Socket, io } from 'socket.io-client'
 import type { ClientToServerEvents, ServerToClientEvents } from './model'
-let a = SOCKET_URL
+
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-	'https://tictactoe-backend-theta.vercel.app/socket.io/?EIO=4&transport=websocket',
+	SOCKET_URL,
 	{
 		autoConnect: false,
 		transports: ['websocket'],
