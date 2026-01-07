b35bd869448c5c35fc86c5bb3d53b723b518e6f8
DeadInsideDi <matvei778990@gmail.com>
2026-01-07 17:42:38 +0300

    -lazy loading

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,49 +1,16 @@
+import { FriendsPage } from '@/pages/FriendsPage'
+import { GameHistoryPage } from '@/pages/GameHistoryPage'
+import { GamePage } from '@/pages/GamePage'
+import { HomePage } from '@/pages/HomePage'
+import { LoginPage } from '@/pages/LoginPage'
+import { OnboardingPage } from '@/pages/OnboardingPage'
+import { OnlinePage } from '@/pages/OnlinePage'
+import { ScoreboardPage } from '@/pages/ScoreboardPage'
 import { ROUTE_PAGES } from '@/shared/config'
-import { lazy } from 'react'
 import { createBrowserRouter, Outlet } from 'react-router'
 import { RootLayout } from '../layouts'
 import { useOnboardingStore } from '../store'
 import { AuthGuard, PublicGuard } from './guards'
-
-const OnboardingPage = lazy(async () => {
-	const module = await import('@/pages/OnboardingPage')
-	return { default: module.OnboardingPage }
-})
-
-const LoginPage = lazy(async () => {
-	const module = await import('@/pages/LoginPage')
-	return { default: module.LoginPage }
-})
-
-const GameHistoryPage = lazy(async () => {
-	const module = await import('@/pages/GameHistoryPage')
-	return { default: module.GameHistoryPage }
-})
-
-const ScoreboardPage = lazy(async () => {
-	const module = await import('@/pages/ScoreboardPage')
-	return { default: module.ScoreboardPage }
-})
-
-const OnlinePage = lazy(async () => {
-	const module = await import('@/pages/OnlinePage')
-	return { default: module.OnlinePage }
-})
-
-const FriendsPage = lazy(async () => {
-	const module = await import('@/pages/FriendsPage')
-	return { default: module.FriendsPage }
-})
-
-const HomePage = lazy(async () => {
-	const module = await import('@/pages/HomePage')
-	return { default: module.HomePage }
-})
-
-const GamePage = lazy(async () => {
-	const module = await import('@/pages/GamePage')
-	return { default: module.GamePage }
-})
 
 export const router = createBrowserRouter([
 	{
